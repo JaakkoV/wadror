0604399f5f6bb5be010199565a19dd31cdb82e4c
JaakkoV <jaakvirt@cs.helsinki.fi>
2017-01-29 21:50:06 +0200

    tehtäviä

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -6,4 +6,8 @@ class Beer < ActiveRecord::Base
     ratings = Rating.where beer_id:self.id
     ratings.average(:score).to_f.round 2
   end
+
+  def to_s
+    "#{self.name}, #{self.brewery.name}"
+  end
 end
