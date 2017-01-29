41f73d0aa428c566e6f8a433a2f7f676004f9429
JaakkoV <jaakvirt@cs.helsinki.fi>
2017-01-29 21:20:26 +0200

    c

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'breweries#index'
   get 'kaikki_bisset', to: 'beers#index'
   get 'ratings', to: 'ratings#index'
+  get 'ratings/new', to:'ratings#new'
+  post 'ratings', to: 'ratings#create'
 
   resources :beers
   resources :breweries
